094cd6066ba1ecbc35ccec94fb0f3f2d11a65982
neuzuratechnologies <neuzuratechnologies@gmail.com>
2026-07-16 18:31:45 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import AppRouter from "./router/AppRouter";
 
-createRoot(document.getElementById('root')!).render(
+AOS.init({
+  duration: 800,
+  easing: "ease-out",
+  once: true,
+  offset: 60,
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppRouter />
   </StrictMode>
